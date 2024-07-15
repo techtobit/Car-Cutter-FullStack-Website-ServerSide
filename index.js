fea5bbaee9eb837ea211a3b11e0dae0a659fb872
techtobit <nurthern2016@gmail.com>
2024-07-16 02:35:28 +0600

    updated url

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
 
  try {
-  await client.connect();
+  // await client.connect();
   const collection = client.db('assainment11').collection("items");
 
   // JWT Login Auth
